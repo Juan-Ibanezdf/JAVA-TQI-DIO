7d7b67e76573858457c8a10db6795fd6ed525c14
Juan.Ibanezdf <juan.ibanezdf@gmail.com>
2022-05-22 16:23:12 -0300

    criando a resposta do primeiro desafio

---
+++ desafio-atividades-java/proibido_a_entrada_de_menores.java
@@ -0,0 +1,42 @@
+// Não esquecer de mudar o nome do arquivo.
+
+import java.util.*;
+ 
+    public class DIO{
+
+	//complete o código para que ele funcione corretamente
+ 
+        public static void main(String[] args) {
+           
+	    //a classe Scanner auxilia na leitura dos dados de entrada
+            Scanner scan = new Scanner(System.in);
+ 
+            int N= scan.nextInt();
+            String string = scan.nextLine();
+	    
+	    
+	    //quebra string em várias substrings a partir de um caracter
+            String[] s = string.split(" ");
+ 
+            double[] idade = new double[N];
+ 
+            for (int i = 0; i < N; i++)
+            
+            {
+                string = scan.nextLine();
+                idade[i] =  Integer.parseInt(string);
+            }
+ 
+ 
+            System.out.println("Nao poderao entrar as idades: ");
+            for (int i = 0; i < N; i++)
+            {
+                if (idade[i] < 18    )
+                {
+                    System.out.println((int)  idade[i]);
+                }
+ 
+            }
+                   
+        }
+    }
